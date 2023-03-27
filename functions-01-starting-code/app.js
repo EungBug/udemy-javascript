d013a88d73cbdf04af4076715b1880b901eb7cb4
EungBug <eungb14@gmail.com>
2023-03-27 20:13:26 +0900

    [📒] JS - 함수 스터디
    함수 선언, 함수 표현, 익명 함수, 화살표 함수 관련 예제

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,63 @@
+const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_CHOICE = 'ROCK';
+
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PALYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_CHOICE} for you!`);
+    return DEFAULT_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) => {
+  return cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) || (cChoice === PAPER && pChoice === SCISSORS) || (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+  // if (cChoice === pChoice) {
+  //   return RESULT_DRAW;
+  // } else if ((cChoice === ROCK && pChoice === PAPER) || (cChoice === PAPER && pChoice === SCISSORS) || (cChoice === SCISSORS && pChoice === ROCK)) {
+  //   return RESULT_PLAYER_WINS;
+  // } else {
+  //   return RESULT_COMPUTER_WINS;
+  // }
+};
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is Starting...');
+  const playerChocie = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+
+  console.log(`computer : ${computerChoice}`);
+  console.log(`player : ${playerChocie}`);
+
+  const winner = getWinner(computerChoice, playerChocie);
+  console.log(winner);
+});
