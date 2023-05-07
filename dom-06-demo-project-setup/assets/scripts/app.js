d998535874902c0ac030c919a721c83a9543688a
EungBug <eungb14@gmail.com>
2023-05-08 01:49:11 +0900

    Study DOM - Add Movie Inpo Handler

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,6 +2,10 @@ const startAddMovieBtn = document.querySelector('header button');
 const addModal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieBtn = addModal.querySelector('.btn--passive');
+const confrimAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addModal.querySelectorAll('input');
+
+const movies = [];
 
 const toggleMovieModal = () => {
   addModal.classList.toggle('visible');
@@ -18,8 +22,44 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieClickHandler = () => {
   toggleMovieModal();
+  clearMovieInputs();
+};
+
+const clearMovieInputs = () => {
+  for (const input of userInputs) {
+    input.value = '';
+  }
+};
+
+const addMovieClickHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    Number(ratingValue) < 1 ||
+    Number(ratingValue) > 5
+  ) {
+    alert('Please enter valid values (rating between 1 and 5).');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInputs();
 };
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
 cancelAddMovieBtn.addEventListener('click', cancelAddMovieClickHandler);
+confrimAddMovieBtn.addEventListener('click', addMovieClickHandler);
