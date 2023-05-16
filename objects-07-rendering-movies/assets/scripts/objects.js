eb3593377f29324fc4aa49c9cc7f28293998743a
EungBug <eungb14@gmail.com>
2023-05-16 23:40:05 +0900

    Study Object & this

--- objects-07-rendering-movies/assets/scripts/objects.js
+++ objects-07-rendering-movies/assets/scripts/objects.js
@@ -18,8 +18,14 @@ const renderMovies = (filter = '') => {
   filteredMovies.forEach(movie => {
     const movieEl = document.createElement('li');
     const { info } = movie;
-    const { title: movieTitle } = info;
-    let text = movieTitle + ' - ';
+    // 구조분해를 통해 얻어온 메서드를 호출할 경우 this가 달라져 정상 동작을 하지 않음
+    // this.info가 undefined를 반환하여 오류 발생
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle() => 전역 실행 컨텍스트 (여기서 this는 window 객체를 참조한다.)
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    // let text = getFormattedTitle() + '-';
+
+    let text = movie.getFormattedTitle() + ' - ';
     // For in 반복문을 통해 사용자 지정 Key 프로퍼티에 접근할 수 있다.
     for (const key in info) {
       if (key !== 'title') {
@@ -45,7 +51,10 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue
     },
-    id: Math.random()
+    id: Math.random().toString(),
+    getFormattedTitle: function () {
+      return this.info.title.toUpperCase();
+    }
   };
 
   movies.push(newMovie);
